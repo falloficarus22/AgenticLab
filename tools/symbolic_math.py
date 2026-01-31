1d8e19a7a8d7e829c0cd3891c290ad7e808393b3
Abhishek <norizzabhii@gmail.com>
2026-01-31 16:25:30 +0000

    Very little changes, didn't get much chance to code today.

--- tools/symbolic_math.py
+++ tools/symbolic_math.py
@@ -1,19 +1,6 @@
 import sympy as sp
-from core.tools import Tool
+import json
 
-class SymbolicMathTool(Tool):
+class SymbolicMathSolver(Tool):
     
-    name = "symbolic_math"
-    description = "Simplify or solve symbolic math expressions"
-    input_schema = {
-        'expression': 'string'
-    }
-
-    def run(self, inputs):
-        expr = sp.sympify(inputs['expression'])
-        simplified = sp.simplify(expr)
-
-        return {
-            'simplified': str(simplified)
-        }
-        
+    def __init__(self, )
